f18d113b6f9fe4d226298e951bb9be5930cfb40d
Shahd <shahad9mubarak@gmail.com>
2026-05-10 12:09:47 +0400

    Override displayInfo() to include patient-specific details

--- src/Entity/Patient.java
+++ src/Entity/Patient.java
@@ -5,14 +5,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Patient extends Person {
-    String patientId;
-    String bloodGroup;
-    List<String> allergies = new ArrayList<>();
-    String emergencyContact;
-    LocalDate registrationDate;
-    String insuranceId;
-    List<String> medicalRecords = new ArrayList<>();
-    List<String> appointments = new ArrayList<>();
+   private String patientId;
+    private String bloodGroup;
+    private List<String> allergies = new ArrayList<>();
+    private String emergencyContact;
+   private LocalDate registrationDate;
+    private String insuranceId;
+   private List<String> medicalRecords = new ArrayList<>();
+    private List<String> appointments = new ArrayList<>();
 
     public Patient(String id, String firstName, String lastName, LocalDate dateOfBirth, String gender, String phoneNumber, String email, String address, LocalDate registrationDate, String patientId, List<String> medicalRecords, String insuranceId, String emergencyContact, String bloodGroup, List<String> appointments, List<String> allergies) {
         super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address);
