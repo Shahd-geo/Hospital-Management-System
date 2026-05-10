535250bb9f639026255c88fb03b8cecd73a334b5
Shahd <shahad9mubarak@gmail.com>
2026-05-10 14:37:38 +0400

    Create Department Class

--- src/Entity/Department.java
+++ src/Entity/Department.java
@@ -1,4 +1,16 @@
 package Entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Department {
+    private String departmentId;
+    private String departmentName;
+    private String headDoctorId;
+    private List<String> doctors = new ArrayList<>();
+    private List<String> nurses = new ArrayList<>();
+    private int bedCapacity;
+    private int availableBeds;
+
+
 }
