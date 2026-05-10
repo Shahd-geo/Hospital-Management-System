e16ca0ec893c279d9cf77251e2df056ac21782eb
Shahd <shahad9mubarak@gmail.com>
2026-05-10 14:31:54 +0400

    Add method  cancel()

--- src/Entity/Appointment.java
+++ src/Entity/Appointment.java
@@ -102,5 +102,8 @@ public class Appointment {
         this.appointmentTime=newTime;
         this.status="Rescheduled";
     }
+    public void  cancel(){
+        this.status="Cancelled";
+    }
 }
 
