60fb5330340191121d976713399b3c4cc0f10006
Shahd <shahad9mubarak@gmail.com>
2026-05-10 11:58:58 +0400

    import java.util.ArrayList;

--- src/Entity/Patient.java
+++ src/Entity/Patient.java
@@ -1,7 +1,11 @@
 package Entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Patient extends Person {
     String patientId;
     String bloodGroup;
+    List<String> allergies = new ArrayList<>();
     L
 }
