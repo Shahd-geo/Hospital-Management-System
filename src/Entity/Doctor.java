5e00c7b5e47c49348ec9ed80cefeaa2ded4f000d
Shahd <shahad9mubarak@gmail.com>
2026-05-10 12:30:06 +0400

    Add specific fields

--- src/Entity/Doctor.java
+++ src/Entity/Doctor.java
@@ -1,11 +1,17 @@
 package Entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Doctor extends Person{
     private String doctorId;
     private String specialization;
+    private String qualification;
    private int experienceYears;
    private String departmentId;
    private Double consultationFee;
+   private List<String> availableSlots =new ArrayList<>();
+   private List<String> assignedPatients;
 
 
 }
