94a74bb51c9ddbc124381e03c8c6e8108a7d08e8
Shahd <shahad9mubarak@gmail.com>
2026-05-10 15:43:19 +0400

    add getDoctorsBySpecialization(String specialization)  method

--- src/Service/DoctorService.java
+++ src/Service/DoctorService.java
@@ -8,12 +8,14 @@ import java.util.List;
 
 public class DoctorService {
     private static List<Doctor> doctors = new ArrayList<>();
-    public void aaddDoctor(Doctor d){
+    public void addDoctor(Doctor d){
         doctors.add(d);
+        System.out.println("----------------");
     }
     public void displayAllDoctors() {
         for(Doctor d: doctors){
             d.displayInfo();
+            System.out.println("----------------");
         }
     }
     public Doctor getDoctorById(String doctorId){
@@ -30,6 +32,24 @@ public class DoctorService {
             doctors.remove(d);
         }
     }
+    public void editDoctor(String doctorId, Doctor updatedDoctor) {
+        Doctor d = getDoctorById(doctorId);
+        if (d != null) {
+            doctors.remove(d);
+            doctors.add(updatedDoctor);
+            System.out.println("----------------");
+        }
+    }
+    public  void getDoctorsBySpecialization(String specialization){
+        for (Doctor d : doctors){
+            if (d.getSpecialization()
+                    .equalsIgnoreCase(specialization)){
+                d.displayInfo();
+
+                System.out.println("----------------");
+            }
+        }
+
 
     }
 
