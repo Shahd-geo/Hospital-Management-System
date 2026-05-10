dcc210c9078c99828f14efca71c5146be91e31c1
Shahd <shahad9mubarak@gmail.com>
2026-05-10 15:58:46 +0400

    add nurse list

--- src/Service/NurseService.java
+++ src/Service/NurseService.java
@@ -1,4 +1,13 @@
 package Service;
 
+import Entity.Doctor;
+import Entity.Nurse;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class NurseService {
+    private static List<Nurse> nurses = new ArrayList<>();
+
+
 }
