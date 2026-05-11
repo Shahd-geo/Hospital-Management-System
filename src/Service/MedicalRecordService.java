f8d8e0e0d0b0f910d07fa859284313ef76456bc8
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-05-11 14:35:25 +0400

    add scaner to take input from user

---
+++ src/Service/MedicalRecordService.java
@@ -0,0 +1,13 @@
+package Service;
+
+import Entity.MedicalRecord;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class MedicalRecordService {
+    Scanner scanner = new Scanner(System.in);
+    static List<MedicalRecord> medicalRecordList = new ArrayList<>();
+
+}
