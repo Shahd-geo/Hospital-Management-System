4eda4e09fa7f18bbec4c53489cc171f04ae3115f
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-05-11 11:59:47 +0400

    add menu massage

--- src/MenuMessages.java
+++ src/MenuMessages.java
@@ -1,3 +1,20 @@
 public class MenuMessages {
+    public static void mainMenu(){
+
+        System.out.println("""
+                
+                ===== Hospital Management System =====
+                
+                1. Patient Menu
+                2. Doctor Menu
+                3. Nurse Menu
+                4. Appointment Menu
+                5. Medical Record Menu
+                6. Department Menu
+                7. Exit
+                
+                """);
+    }
+
 
 }
