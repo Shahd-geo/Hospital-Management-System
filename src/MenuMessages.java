9e84dc3ddb34808ac9d3be1a34daffdb7d7368a2
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-05-11 12:03:29 +0400

    add menu patient  massage

--- src/MenuMessages.java
+++ src/MenuMessages.java
@@ -1,20 +1,31 @@
 public class MenuMessages {
-    public static void mainMenu(){
-
-        System.out.println("""
-                
-                ===== Hospital Management System =====
-                
-                1. Patient Menu
-                2. Doctor Menu
-                3. Nurse Menu
-                4. Appointment Menu
-                5. Medical Record Menu
-                6. Department Menu
-                7. Exit
-                
-                """);
-    }
+    public static String MAIN_MENU_MESSAGE = """
+            
+            ===== Hospital Management System =====
+            
+            1- Patient Menu
+            2- Doctor Menu
+            3- Nurse Menu
+            4- Appointment Menu
+            5- Medical Record Menu
+            6- Department Menu
+            7- Exit
+            
+            """;
 
 
+    public static String PATIENT_MENU_MESSAGE = """
+            
+            ===== Patient Menu =====
+            
+            1- Add Patient
+            2- Update Patient
+            3- Remove Patient
+            4- Get Patient By ID 
+            5-Display All Patients
+            6- Search Patient By Name
+            7- Go Back To Main Menu
+            
+            """;
 }
+
