8bab9ed7dc2f9f29a594692f3832ece43bc42cfe
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-05-11 15:59:23 +0400

    create scanner to take inout from user

--- src/Service/DepartmentService.java
+++ src/Service/DepartmentService.java
@@ -1,4 +1,11 @@
 package Service;
 
+import Entity.Department;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 public class DepartmentService {
+    Scanner scanner = new Scanner(System.in);
+    static List<Department> departmentList = new ArrayList<>();
 }
