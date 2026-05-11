e44ba779e6ef996b3643e841824b72a20282b9d1
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-05-11 10:02:11 +0400

    add dector services

---
+++ src/Service/DoctorService.java
@@ -0,0 +1,17 @@
+package Service;
+
+import Entity.Doctor;
+import Entity.Patient;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class DoctorService {
+    static Scanner scanner = new Scanner(System.in);
+    public List <Doctor>  doctors=new ArrayList<>();
+
+
+    public static Doctor addDector() {
+
+}
