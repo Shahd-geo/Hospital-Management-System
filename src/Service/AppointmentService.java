d32c1969f2b33e2aa43b6a6ffb026e76008ff223
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-05-11 15:07:17 +0400

    create scanner to take inout from user

--- src/Service/AppointmentService.java
+++ src/Service/AppointmentService.java
@@ -1,4 +1,12 @@
 package Service;
 
+import Entity.Appointment;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class AppointmentService {
+    static List<Appointment> appointmentList = new ArrayList<>();
+    Scanner scanner = new Scanner(System.in);
 }
