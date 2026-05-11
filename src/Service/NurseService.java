8c36d280a6f3c7ee9ac153db8f7211b4129ef28e
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-05-11 13:51:28 +0400

    creat nurse list

---
+++ src/Service/NurseService.java
@@ -0,0 +1,17 @@
+package Service;
+
+import Entity.Nurse;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class NurseService {
+    static Scanner scanner = new Scanner(System.in);
+    static List<Nurse> nurseList = new ArrayList<>();
+    List<String> assignedPatients = new ArrayList<>();
+
+
+    public
+
+}
