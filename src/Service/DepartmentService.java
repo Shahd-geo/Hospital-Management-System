cc8e56ac291d3f30000c6d83c0784bc5de87977f
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-05-11 16:00:23 +0400

    take department  details

--- src/Service/DepartmentService.java
+++ src/Service/DepartmentService.java
@@ -8,4 +8,17 @@ import java.util.Scanner;
 public class DepartmentService {
     Scanner scanner = new Scanner(System.in);
     static List<Department> departmentList = new ArrayList<>();
+
+
+    public Department addDepartment(){
+        System.out.println("Enter Department Id :");
+        String departmentId = scanner.nextLine();
+        System.out.println("Enter department Name :");
+        String departmentName = scanner.nextLine();
+        System.out.println("Enter department head DoctorId :");
+        String headDoctorId = scanner.nextLine();
+        System.out.println("Enter department  bed Capacity :");
+        int bedCapacity = scanner.nextInt();
+        System.out.println("Enter department available Beds :");
+        int availableBeds = scanner.nextInt();
 }
