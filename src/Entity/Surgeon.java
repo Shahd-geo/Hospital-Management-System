001b5d5320e4846e2648210139204bc133bfb102
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-05-12 08:31:29 +0400

    Additional fields

--- src/Entity/Surgeon.java
+++ src/Entity/Surgeon.java
@@ -1,4 +1,12 @@
 package Entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Surgeon extends Doctor {
+    int surgeriesPerformed;
+    List<String>surgeryTypes=new ArrayList<>();
+    boolean operationTheatreAccess;
+
+
 }
