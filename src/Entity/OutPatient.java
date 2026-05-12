bed785b24b2e6618270877c5f63d0e50ed4105d2
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-05-12 09:52:25 +0400

    override display info

--- src/Entity/OutPatient.java
+++ src/Entity/OutPatient.java
@@ -8,35 +8,43 @@ public class OutPatient extends Patient{
     LocalDate lastVisitDate;
     String preferredDoctorId;
 
-    public int getVisitCount() {
-        return visitCount;
-    }
-
-    public void setVisitCount(int visitCount) {
-        this.visitCount = visitCount;
-    }
-
-    public LocalDate getLastVisitDate() {
-        return lastVisitDate;
-    }
-
-    public void setLastVisitDate(LocalDate lastVisitDate) {
-        this.lastVisitDate = lastVisitDate;
-    }
-
-    public String getPreferredDoctorId() {
-        return preferredDoctorId;
-    }
-
-    public void setPreferredDoctorId(String preferredDoctorId) {
-        this.preferredDoctorId = preferredDoctorId;
-    }
 
     public OutPatient(String id, String firstName, String lastName, LocalDate dateOfBirth, String gender, String phoneNumber, String email, String address, LocalDate registrationDate, String patientId, List<MedicalRecord> medicalRecords, String insuranceId, String emergencyContact, String bloodGroup, List<Appointment> appointments, List<String> allergies, int visitCount, LocalDate lastVisitDate, String preferredDoctorId) {
         super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, registrationDate, patientId, medicalRecords, insuranceId, emergencyContact, bloodGroup, appointments, allergies);
         this.visitCount = visitCount;
         this.lastVisitDate = lastVisitDate;
         this.preferredDoctorId = preferredDoctorId;
-
     }
+        public int getVisitCount() {
+            return visitCount;
+        }
+
+        public void setVisitCount(int visitCount) {
+            this.visitCount = visitCount;
+        }
+
+        public LocalDate getLastVisitDate() {
+            return lastVisitDate;
+        }
+
+        public void setLastVisitDate(LocalDate lastVisitDate) {
+            this.lastVisitDate = lastVisitDate;
+        }
+
+        public String getPreferredDoctorId() {
+            return preferredDoctorId;
+        }
+
+        public void setPreferredDoctorId(String preferredDoctorId) {
+            this.preferredDoctorId = preferredDoctorId;
+        }
+        @Override
+        public void displayInfo() {
+            super.displayInfo();
+            System.out.println("Visit Count        : " + visitCount);
+            System.out.println("Last Visit Date    : " + lastVisitDate);
+            System.out.println("Preferred DoctorId : " + preferredDoctorId);
+
+
+        }
 }
