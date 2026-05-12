98566f45a5261c151b2c9b6150066bd915563e09
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-05-12 09:38:50 +0400

    add Additional fields:

--- src/Entity/InPatient.java
+++ src/Entity/InPatient.java
@@ -1,4 +1,12 @@
 package Entity;
 
+import java.time.LocalDate;
+
 public class InPatient extends Patient {
+    private LocalDate admissionDate;
+    private LocalDate dischargeDate;
+    private String roomNumber;
+    private String bedNumber;
+    private String admittingDoctorId;
+
 }
