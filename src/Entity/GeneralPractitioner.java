2cf3005b6d75f2d86cacc89e8593cf54f883d018
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-05-12 09:22:06 +0400

    add constracter

--- src/Entity/GeneralPractitioner.java
+++ src/Entity/GeneralPractitioner.java
@@ -1,7 +1,17 @@
 package Entity;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public class GeneralPractitioner  extends Doctor{
    private boolean walkinAvailable;
    private boolean homeVisitAvailable;
   private   boolean vaccinationCertified;
+
+    public GeneralPractitioner(String id, String firstName, String lastName, LocalDate dateOfBirth, String gender, String phoneNumber, String email, String address, String doctorId, String specialization, String qualification, int experienceYears, String departmentId, Double consultationFee, List<String> availableSlots, List<String> assignedPatients, boolean walkinAvailable, boolean homeVisitAvailable, boolean vaccinationCertified) {
+        super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, doctorId, specialization, qualification, experienceYears, departmentId, consultationFee, availableSlots, assignedPatients);
+        this.walkinAvailable = walkinAvailable;
+        this.homeVisitAvailable = homeVisitAvailable;
+        this.vaccinationCertified = vaccinationCertified;
+    }
 }
