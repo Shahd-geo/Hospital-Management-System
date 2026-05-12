eb7812122dadd0f3cdd8cde47041c7253988bc4b
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-05-12 10:11:18 +0400

    add getter and setter

--- src/Entity/EmergencyPatient.java
+++ src/Entity/EmergencyPatient.java
@@ -1,7 +1,51 @@
 package Entity;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public class EmergencyPatient extends Patient{
    private String emergencyType;
    private String arrivalMode;
+   private  int triageLevel;
+   private boolean admittedViaER;
 
+    public EmergencyPatient(String id, String firstName, String lastName, LocalDate dateOfBirth, String gender, String phoneNumber, String email, String address, LocalDate registrationDate, String patientId, List<MedicalRecord> medicalRecords, String insuranceId, String emergencyContact, String bloodGroup, List<Appointment> appointments, List<String> allergies, String emergencyType, String arrivalMode, int triageLevel, boolean admittedViaER) {
+        super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, registrationDate, patientId, medicalRecords, insuranceId, emergencyContact, bloodGroup, appointments, allergies);
+        this.emergencyType = emergencyType;
+        this.arrivalMode = arrivalMode;
+        this.triageLevel = triageLevel;
+        this.admittedViaER = admittedViaER;
+    }
+
+    public String getEmergencyType() {
+        return emergencyType;
+    }
+
+    public void setEmergencyType(String emergencyType) {
+        this.emergencyType = emergencyType;
+    }
+
+    public String getArrivalMode() {
+        return arrivalMode;
+    }
+
+    public void setArrivalMode(String arrivalMode) {
+        this.arrivalMode = arrivalMode;
+    }
+
+    public int getTriageLevel() {
+        return triageLevel;
+    }
+
+    public void setTriageLevel(int triageLevel) {
+        this.triageLevel = triageLevel;
+    }
+
+    public boolean isAdmittedViaER() {
+        return admittedViaER;
+    }
+
+    public void setAdmittedViaER(boolean admittedViaER) {
+        this.admittedViaER = admittedViaER;
+    }
 }
