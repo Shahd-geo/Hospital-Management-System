9ce5489b85fb194256e13e11f666faf950371371
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-05-12 09:01:30 +0400

    add getter and setter

--- src/Entity/Consultant.java
+++ src/Entity/Consultant.java
@@ -1,5 +1,6 @@
 package Entity;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,5 +9,34 @@ public class Consultant extends Doctor{
     private boolean onlineConsultationAvailable;
    private int consultationDuration; //(int - in minutes)
 
+    public Consultant(String id, String firstName, String lastName, LocalDate dateOfBirth, String gender, String phoneNumber, String email, String address, String doctorId, String specialization, String qualification, int experienceYears, String departmentId, Double consultationFee, List<String> availableSlots, List<String> assignedPatients, List<String> consultationTypes, boolean onlineConsultationAvailable, int consultationDuration) {
+        super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, doctorId, specialization, qualification, experienceYears, departmentId, consultationFee, availableSlots, assignedPatients);
+        this.consultationTypes = consultationTypes;
+        this.onlineConsultationAvailable = onlineConsultationAvailable;
+        this.consultationDuration = consultationDuration;
+    }
 
+    public List<String> getConsultationTypes() {
+        return consultationTypes;
+    }
+
+    public void setConsultationTypes(List<String> consultationTypes) {
+        this.consultationTypes = consultationTypes;
+    }
+
+    public boolean isOnlineConsultationAvailable() {
+        return onlineConsultationAvailable;
+    }
+
+    public void setOnlineConsultationAvailable(boolean onlineConsultationAvailable) {
+        this.onlineConsultationAvailable = onlineConsultationAvailable;
+    }
+
+    public int getConsultationDuration() {
+        return consultationDuration;
+    }
+
+    public void setConsultationDuration(int consultationDuration) {
+        this.consultationDuration = consultationDuration;
+    }
 }
