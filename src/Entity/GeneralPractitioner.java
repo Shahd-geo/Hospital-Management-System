87f0903658ef45bf42ac0c02d92cecb39851ed19
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-05-12 09:20:54 +0400

    add Additional fields in GeneralPractitioner Class

---
+++ src/Entity/GeneralPractitioner.java
@@ -0,0 +1,7 @@
+package Entity;
+
+public class GeneralPractitioner  extends Doctor{
+   private boolean walkinAvailable;
+   private boolean homeVisitAvailable;
+  private   boolean vaccinationCertified;
+}
