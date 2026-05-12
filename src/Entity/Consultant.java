79e0db2408eb0c551102306f1b50cded70ed27bd
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-05-12 09:00:40 +0400

    add Additional fields

--- src/Entity/Consultant.java
+++ src/Entity/Consultant.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Consultant extends Doctor{
    private List<String>consultationTypes=new ArrayList<>();
     private boolean onlineConsultationAvailable;
-   private int consultationDuration;
+   private int consultationDuration; //(int - in minutes)
 
 
 }
