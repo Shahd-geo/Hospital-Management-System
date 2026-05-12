04b8706ae6721450152e9439e722641df16f2fdc
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-05-12 08:32:07 +0400

    Additional fields

--- src/Entity/Surgeon.java
+++ src/Entity/Surgeon.java
@@ -1,5 +1,6 @@
 package Entity;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,5 +9,10 @@ public class Surgeon extends Doctor {
     List<String>surgeryTypes=new ArrayList<>();
     boolean operationTheatreAccess;
 
-
+    public Surgeon(String id, String firstName, String lastName, LocalDate dateOfBirth, String gender, String phoneNumber, String email, String address, String doctorId, String specialization, String qualification, int experienceYears, String departmentId, Double consultationFee, List<String> availableSlots, List<String> assignedPatients, int surgeriesPerformed, List<String> surgeryTypes, boolean operationTheatreAccess) {
+        super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, doctorId, specialization, qualification, experienceYears, departmentId, consultationFee, availableSlots, assignedPatients);
+        this.surgeriesPerformed = surgeriesPerformed;
+        this.surgeryTypes = surgeryTypes;
+        this.operationTheatreAccess = operationTheatreAccess;
+    }
 }
