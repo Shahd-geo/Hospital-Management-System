5ae21412285bfd4001a08493fce1edc49c49b9fd
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-05-12 09:41:13 +0400

    add getter and setter

--- src/Entity/InPatient.java
+++ src/Entity/InPatient.java
@@ -1,6 +1,7 @@
 package Entity;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class InPatient extends Patient {
     private LocalDate admissionDate;
@@ -10,4 +11,61 @@ public class InPatient extends Patient {
     private String admittingDoctorId;
     private double dailyCharges;
 
+    public InPatient(String id, String firstName, String lastName, LocalDate dateOfBirth, String gender, String phoneNumber, String email, String address, LocalDate registrationDate, String patientId, List<MedicalRecord> medicalRecords, String insuranceId, String emergencyContact, String bloodGroup, List<Appointment> appointments, List<String> allergies, LocalDate admissionDate, LocalDate dischargeDate, String roomNumber, String bedNumber, String admittingDoctorId, double dailyCharges) {
+        super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, registrationDate, patientId, medicalRecords, insuranceId, emergencyContact, bloodGroup, appointments, allergies);
+        this.admissionDate = admissionDate;
+        this.dischargeDate = dischargeDate;
+        this.roomNumber = roomNumber;
+        this.bedNumber = bedNumber;
+        this.admittingDoctorId = admittingDoctorId;
+        this.dailyCharges = dailyCharges;
+    }
+
+    public LocalDate getAdmissionDate() {
+        return admissionDate;
+    }
+
+    public void setAdmissionDate(LocalDate admissionDate) {
+        this.admissionDate = admissionDate;
+    }
+
+    public LocalDate getDischargeDate() {
+        return dischargeDate;
+    }
+
+    public void setDischargeDate(LocalDate dischargeDate) {
+        this.dischargeDate = dischargeDate;
+    }
+
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(String roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public String getBedNumber() {
+        return bedNumber;
+    }
+
+    public void setBedNumber(String bedNumber) {
+        this.bedNumber = bedNumber;
+    }
+
+    public String getAdmittingDoctorId() {
+        return admittingDoctorId;
+    }
+
+    public void setAdmittingDoctorId(String admittingDoctorId) {
+        this.admittingDoctorId = admittingDoctorId;
+    }
+
+    public double getDailyCharges() {
+        return dailyCharges;
+    }
+
+    public void setDailyCharges(double dailyCharges) {
+        this.dailyCharges = dailyCharges;
+    }
 }
