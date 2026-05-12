2998f2b993362a686cba41253dbe58c5f6028fa0
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-05-12 08:58:21 +0400

    add Additional fields

--- src/Entity/Consultant.java
+++ src/Entity/Consultant.java
@@ -1,4 +1,11 @@
 package Entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Consultant extends Doctor{
+    List<String>consultationTypes=new ArrayList<>();
+    boolean onlineConsultationAvailable;
+
+
 }
