b2a2d6de5de2c72e53f2ccb522fdaeb100b8e0ef
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-05-14 12:48:06 +0400

    CREATE dailyAppointmentsReport METHOD

--- src/Service/ReportService.java
+++ src/Service/ReportService.java
@@ -1,4 +1,25 @@
 package Service;
 
+import Entity.Appointment;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public class ReportService {
+    public static void dailyAppointmentsReport(List<Appointment> appointments, LocalDate date) {
+        System.out.println("===== Daily Appointments Report =====");
+
+        boolean found = false;
+
+        for (Appointment a : appointments) {
+            if (a.getAppointmentDate().equals(date)) {
+                a.displayInfo();
+                found = true;
+            }
+        }
+
+        if (!found) {
+            System.out.println("No appointments found for this date.");
+        }
+    }
 }
